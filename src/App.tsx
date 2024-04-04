9db523920317dea00edf7a15eb7d33f971cb58b3
kprzybyl <katarzynaa.przybyl@gmail.com>
2024-04-04 13:37:31 +0200

    Install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export const App = () => {
-  return <div>Lista tagów</div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-slate-200">
+      <p className="text-3xl font-bold">Lista tagów</p>
+    </div>
+  );
 };
